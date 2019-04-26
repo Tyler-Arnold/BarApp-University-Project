e133664cb6ef6344a45744679e7cf10165d6cb28
Tyler Arnold <ArnoldTyler1998@gmail.com>
2019-04-26 23:47:34 +0100

    Nav drawer now actually works

--- app/src/main/java/uk/ac/bmth/icecodetruckers/barapp/MainActivity.kt
+++ app/src/main/java/uk/ac/bmth/icecodetruckers/barapp/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+
+
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
@@ -16,6 +20,10 @@ class MainActivity : AppCompatActivity(),
     CocktailCalculator.OnFragmentInteractionListener,
     CocktailRecipe.OnFragmentInteractionListener {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+
     override fun onFragmentInteraction(uri: Uri) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -24,15 +32,21 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
         findViewById<Toolbar>(R.id.toolbar)
             .setupWithNavController(navController, appBarConfiguration)
+
+
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
-
-
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 
 }
